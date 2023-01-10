cabfa7c8ee02f5e2447a1ab3e9d779ae3fc70845
MAKSIM KAZANTSEV <98515879+maxkeks999@users.noreply.github.com>
2023-01-10 13:53:26 +0700

    Delete C-Seminar_1_HomeWork directory

--- C-Seminar_1_HomeWork/Task4.cs
+++
@@ -1,9 +0,0 @@
-Console.Clear();
-
-Console.Write("Введите число: ");
-string input = Console.ReadLine();
-int number = int.Parse(input);
-for (int i = 2; i <= number; i += 2)
-{
-	Console.Write(i);
-}
