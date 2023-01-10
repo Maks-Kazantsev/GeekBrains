dfb19aa3d0b182350111859ec5cc19e7a369ecba
MAKSIM KAZANTSEV <98515879+maxkeks999@users.noreply.github.com>
2023-01-10 13:52:21 +0700

    Delete C-_Seminar_2_HomeWork directory

--- C-_Seminar_2_HomeWork/Task_1.cs
+++
@@ -1,6 +0,0 @@
-Console.Clear();
-Console.WriteLine("Введите трехзначное число");
-string input = Console.ReadLine();
-int number = int.Parse(input);
-int secondDigit = number % 100 / 10;
-Console.WriteLine($"Вторая цифра числа: {secondDigit}");
