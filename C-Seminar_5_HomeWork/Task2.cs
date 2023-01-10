7cfff8c485f22478639f5885a7af5c828df85a26
MAKSIM KAZANTSEV <98515879+maxkeks999@users.noreply.github.com>
2023-01-10 13:27:55 +0700

    Add files via upload

---
+++ C-Seminar_5_HomeWork/Task2.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+int[] array = CreateRandomArray();
+long sum = OddElementsSum(array);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sum}");
+
+int[] CreateRandomArray()
+{
+	Random rnd = new Random();
+	int size = rnd.Next(1);
+	int[] array = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = rnd.Next();
+	}
+	return array;
+}
+
+long OddElementsSum(int[] array)
+{
+	long sum = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if ((i + 1) % 2 == 1)
+		{
+			sum += array[i];
+		}
+	}
+	return sum;
+}
